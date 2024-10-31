fe8c2a5a168500d587b1f3510ddd977ee8e04a0d
jessekl <jesse.klint.jk@gmail.com>
2024-10-31 18:31:52 +0200

    environment variables

--- server/index.js
+++ server/index.js
@@ -1,62 +1,18 @@
 import express from 'express'
 import cors from 'cors'
-import pkg from 'pg'
+import todoRouter from './routers/todoRouter.js'
 
-const port = 3001
-const { Pool } = pkg
+const port = process.env.PORT
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+app.use('/',todoRouter)
 
-app.get('/', (req, res) => {
-    const pool = openDb()
-    pool.query('SELECT * FROM task', (error, result) => {
-        if (error) {
-            return res.status(500).json({ error: error.message })
-        }
-        return res.status(200).json(result.rows)
-    })
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    res.status(statusCode).json({error: err.message})
 })
-
-app.post('/create', (req, res) => {
-    const pool = openDb()
-
-    pool.query('INSERT INTO task (description) VALUES ($1) RETURNING *',
-        [req.body.description],
-        (error, result) => {
-            if (error) {
-                return res.status(500).json({ error: error.message })
-            }
-            return res.status(200).json({ id: result.rows[0].id })
-        }
-    )
-})
-
-app.delete('/delete/:id', (req, res) => {
-    const pool = openDb()
-    const id = parseInt(req.params.id)
-    pool.query('DELETE FROM task WHERE id = $1',
-        [id],
-        (error, result) => {
-            if (error) {
-                return res.status(500).json({ error: error.message })
-            }
-         return res.status(200).json({ id: id })
-        }
-    )
-})
-
-const openDb = () => {
-    const pool = new Pool({
-        user: 'postgres',
-        host: 'localhost',
-        database: 'todo',
-        password: 'postgres',
-        port: 5432
-    })
-    return pool
-}
 
 app.listen(port)
