8f0244d0a6101ff72c9bccf03346f0be6fcf5b65
jessekl <jesse.klint.jk@gmail.com>
2024-11-04 23:09:52 +0200

    backend auth

--- server/index.test.js
+++ server/index.test.js
@@ -1,10 +1,17 @@
-import { expect } from 'chai'
+import { initializeTestDb, insertTestUser, getToken } from './helper/test.js'
 
 const base_url = 'http://localhost:3001'
 
+import { expect } from 'chai'
+
+
 describe('GET tasks', () => {
+    before(() => {
+        initializeTestDb()
+    })
+
     it('Should get all tasks', async () => {
-        const response = await fetch(base_url)
+        const response = await fetch(base_url + '/')
         const data = await response.json()
 
         expect(response.status).to.equal(200)
@@ -14,11 +21,17 @@ describe('GET tasks', () => {
 })
 
 describe('POST task', () => {
+    const email = 'post@foo.com'
+    const password = 'post123'
+    insertTestUser(email, password)
+    const token = getToken(email)
+
     it('should post a task', async () => {
         const response = await fetch(base_url + '/create', {
             method: 'post',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                Authorization: token
             },
             body: JSON.stringify({ 'description': 'Task from unit test' })
         })
@@ -27,12 +40,12 @@ describe('POST task', () => {
         expect(data).to.be.an('object')
         expect(data).to.include.all.keys('id')
     })
-
     it('should not post a task without description', async () => {
         const response = await fetch(base_url + '/create', {
             method: 'post',
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                Authorization: token
             },
             body: JSON.stringify({ 'description': null })
         })
@@ -44,9 +57,18 @@ describe('POST task', () => {
 })
 
 describe('DELETE task', () => {
+    const email = 'delete@foo.com'
+    const password = 'delete123'
+    insertTestUser(email,password)
+    const token = getToken(email)
+
     it('should delete a task', async () => {
         const response = await fetch(base_url + '/delete/1', {
-            method: 'delete'
+            method: 'delete',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: token
+            }
         })
         const data = await response.json()
         expect(response.status).to.equal(200)
@@ -56,11 +78,52 @@ describe('DELETE task', () => {
 
     it('should not delete a task with SQL injection', async () => {
         const response = await fetch(base_url + '/delete/id=0 or id > 0', {
-            method: 'delete'
+            method: 'delete',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: token
+            }
         })
         const data = await response.json()
         expect(response.status).to.equal(500)
         expect(data).to.be.an('object')
         expect(data).to.include.all.keys('error')
+    })
+})
+
+describe('POST register', () => {
+    const email = 'register@foo.com'
+    const password = 'register123'
+    it('should register with valid email and password', async () => {
+        const response = await fetch(base_url + '/user/register', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'email': email, 'password': password })
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(201, data.error)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('id', 'email')
+    })
+})
+
+describe('POST login', () => {
+    const email = 'login@foo.com'
+    const password = 'login123'
+    insertTestUser(email, password)
+    it('should login with valid credentials', async () => {
+        const response = await fetch(base_url + '/user/login', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'email': email, 'password': password })
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(200, data.error)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('id', 'email', 'token')
     })
 })
--- server/routers/todoRouter.js
+++ server/routers/todoRouter.js
@@ -1,39 +1,34 @@
 import { pool } from '../helper/db.js'
-import e, { Router } from 'express'
+import { Router } from 'express'
 import { emptyOrRows } from '../helper/utils.js'
+import { auth } from '../helper/auth.js'
 
 const router = Router()
 
-router.get('/', (req,res,next) => {
+router.get('/', (req, res, next) => {
     pool.query('SELECT * FROM task', (error, result) => {
-        if (error) {
-            return next(error)
-        }
+        if (error) return next(error)
         return res.status(200).json(emptyOrRows(result))
     })
 })
 
-router.post('/create', (req,res,next) => {
+router.post('/create', auth, (req, res, next) => {
     pool.query('INSERT INTO task (description) VALUES ($1) RETURNING *',
         [req.body.description],
         (error, result) => {
-            if (error) {
-                return next(error)
-            }
+            if (error) return next(error)
             return res.status(200).json({ id: result.rows[0].id })
         }
     )
 })
 
-router.delete('/delete/:id', (req,res,next) => {
+router.delete('/delete/:id', auth, (req, res, next) => {
     const id = parseInt(req.params.id)
     pool.query('DELETE FROM task WHERE id = $1',
         [id],
         (error, result) => {
-            if (error) {
-                return next(error)
-            }
-         return res.status(200).json({ id: id })
+            if (error) return next(error)
+            return res.status(200).json({ id: id })
         }
     )
 })
